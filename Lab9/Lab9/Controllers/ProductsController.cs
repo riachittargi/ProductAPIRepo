0763638ab33e169ac4da1446edce4fd2cf0a43d9
Ria Chittargi <tuo44524@temple.edu>
2025-04-11 14:43:14 -0400

    Updated Products API output for Lab 12: Changed first product values

---
+++ Lab9/Lab9/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Lab9.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab9.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Cheddar Soup", Category = "Soup", Price = 4 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Coffee", Category = "Drinks", Price = 15.36M }
+        };
+        public ProductsController()
+        {
+        }
+        public ProductsController(Product[] products)
+        {
+            this.products = products;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ Lab9/Lab9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab9.WebApiApplication" Language="C#" %>
